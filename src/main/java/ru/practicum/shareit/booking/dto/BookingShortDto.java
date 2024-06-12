60da77bf79bd53257ff9d858a69be0e3c780d752
Maxlimw <maxlimw@yandex.ru>
2024-06-12 22:21:20 +0300

    add-item-request

--- src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
